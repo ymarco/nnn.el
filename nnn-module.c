28e51ea7c9866cc5a20c1108403f1d4ecac380f9
Yoav Marco <yoavm448@gmail.com>
2020-11-06 10:32:07 +0200

    New module functions: get-entry-index, make-extended-context

--- nnn-module.c
+++ nnn-module.c
@@ -1,6 +1,7 @@
 #include <dirent.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
 #include "nnn-module.h"
 
@@ -92,6 +93,34 @@ static emacs_value Fnnn_number_of_entries(emacs_env *env, ptrdiff_t nargs __attr
   return env->make_integer(env, pctx->ndents);
 }
 
+emacs_value Fnnn_get_entry_index(emacs_env *env, ptrdiff_t nargs __attribute__((unused)),
+                                 emacs_value args[], void *data __attribute__((unused))) {
+  context* pctx = env->get_user_ptr(env, args[0]);
+  char path[PATH_MAX];
+  ptrdiff_t pathlen = sizeof(path);
+  env->copy_string_contents(env, args[1], path, &pathlen);
+  return env->make_integer(env, dentfind(pctx, path));
+}
+
+emacs_value Fnnn_make_extended_context(emacs_env *env, ptrdiff_t nargs __attribute__((unused)),
+                                       emacs_value args[], void *data __attribute__((unused))) {
+  context* pctx = env->get_user_ptr(env, args[0]);
+  int i = env->extract_integer(env, args[1]);
+  check_range(env, i, 0, pctx->ndents);
+  char newpath[PATH_MAX];
+  struct entry *pent = &pctx->pdents[i];
+  mkpath(pctx->c_path, pent->name, newpath);
+  if ((pent->flags & DIR_OR_LINK_TO_DIR) && (chdir(newpath) != -1)) {
+    /* Valid dir, make the new context */
+    context *new_pctx = malloc(sizeof(context));
+    if (!new_pctx) return Qnil;
+    if (!make_context(new_pctx, newpath, default_cfg)) return Qnil;
+    return env->make_user_ptr(env, clean_context, new_pctx);
+  } else {
+    return Qnil;
+  }
+}
+
 #define BIND(c_name, emacs_name, arg_min, arg_max, docstring) \
   bind_function(env, emacs_name, \
                 env->make_function(env, arg_min, arg_max, \
@@ -120,6 +149,12 @@ int emacs_module_init(struct emacs_runtime *ert) {
        "Return filename for file entry in context at index i.");
   BIND(Fnnn_number_of_entries, "nnn-number-of-entries", 1, 1,
        "Return the number of file entries in CTX.");
+  BIND(Fnnn_get_entry_index, "nnn-get-entry-index", 2, 2,
+       "Return the index for FILENAME (a small file name with no parent\n"
+       "directories that doesn't end with a slash).");
+  BIND(Fnnn_make_extended_context, "nnn-make-extended-context", 2, 2,
+       "Return a new context corresponding to the subdir of index I at CTX.\n"
+       "If at index I there's no subdir return nil.");
 
 
   // done!
